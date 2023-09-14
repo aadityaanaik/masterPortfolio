d3e89ebd3b20f5efd23b813d0ebcd0baa8bf95c8
naikvaditya <142937246+naikvaditya@users.noreply.github.com>
2023-09-14 14:22:03 -0500

    changes in master

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,6 @@
-git remote remove origin
-rm -rf .git
-npm install && npm run build
-cd build
-git init
-git remote add origin https://github.com/NaikRyder/NaikRyder.github.io.git
 git pull origin master
 git add .
-git commit -m "deploying changes to special repo"
+git commit -m "changes in master"
 git push origin master
-git remote remove origin
-rm -rf git
-cd ..
-git init
-git remote add origin https://github.com/NaikRyder/masterPortfolio.git
+npm install && npm run build
+cp build/* /Users/adityavnaik/IdeaProjects/NaikRyder.github.io/*

e9d089ba4fbcf075acb0bf948fc5fbdfea512d11
naikvaditya <142937246+naikvaditya@users.noreply.github.com>
2023-09-18 12:46:01 -0500

    changes in master

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-git pull origin master
+git status
 git add .
 git commit -m "changes in master"
+git pull origin master
 git push origin master
 npm install && npm run build
 cp -R build/* /Users/adityavnaik/IdeaProjects/NaikRyder.github.io

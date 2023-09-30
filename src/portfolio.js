f73aa068e60ee25ae48d9b5842922bffd6d7b4bb
naikvaditya <142937246+naikvaditya@users.noreply.github.com>
2023-09-30 13:44:00 -0500

    changes in master

--- src/portfolio.js
+++ src/portfolio.js
@@ -47,12 +47,6 @@ const socialMediaLinks = [
     fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
     backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
   },
-  {
-    name: "Instagram",
-    link: "https://www.instagram.com/hey.aditya/",
-    fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
-    backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
-  },
 ];
 
 const skills = {
